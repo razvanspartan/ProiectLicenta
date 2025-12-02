722804993886c383487768dd573104c51795c3c1
razvanspartan <irazinfi@gmail.com>
2025-12-02 13:47:44 +0200

    [feat] Add get all order method in routes.

--- OrderService/app/models/order.py
+++ OrderService/app/models/order.py
@@ -1,11 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-
-db= SQLAlchemy()
-
+from app import db
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(100), nullable=False)
     total = db.Column(db.Float, nullable=False)
+    books = db.relationship('OrderItem', backref='order', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<Order {self.item_name}>'
