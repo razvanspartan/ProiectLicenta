b23519146590fcad0b5389d9eaf186721c3959e4
razvanspartan <irazinfi@gmail.com>
2026-01-02 17:57:46 +0200

    [feat] metric collector done.

--- PredictionSystem/app/interfaces/metric_collector_interface.py
+++ PredictionSystem/app/interfaces/metric_collector_interface.py
@@ -4,13 +4,6 @@ class MetricCollectorInterface(ABC):
     @abstractmethod
     def collect_metrics(self,instance_name: str, metrics: list) -> None:
         pass
-
-    @abstractmethod
-    def get_metrics_window(self) -> list:
-        pass
     @abstractmethod
     def aggregate_metrics(self) -> dict:
-        pass
-    @abstractmethod
-    def get_training_window(self) -> list:
         pass
---
+++ PredictionSystem/app/models/metric_collector.py
@@ -0,0 +1,29 @@
+import time
+
+from app.interfaces.metric_collector_interface import MetricCollectorInterface
+
+
+class MetricCollector(MetricCollectorInterface):
+    def __init__(self, name: str):
+        self.name = name
+        self.window_size_seconds = 5
+        self.instances = {}
+
+    def collect_metric(self, instance_name: str, metrics: dict) -> None:
+        now = time.time()
+        self.instances[instance_name] = {
+            "last_seen": now,
+            "metrics": metrics
+        }
+
+    def aggregate_metrics (self):
+        now = time.time()
+        for instance_name in list(self.instances.keys()):
+            if now - self.instances[instance_name]["last_seen"] > 300:
+                del self.instances[instance_name]
+        cpu_avg = sum(metric["cpu"] for metric in self.instances.values()) / len(self.instances)
+        memory_sum = sum(metric["memory"] for metric in self.instances.values())
+        return {
+            "cpu_avg": cpu_avg,
+            "memory_sum": memory_sum
+        }
